1afda661413202e4b063a71d2af681806a79d88a
Jacob Bremer <bremerjp@gmail.com>
2024-02-12 14:12:26 -0500

    completed basics

---
+++ basics/basics.rb
@@ -0,0 +1,47 @@
+#1
+puts "Jacob" + " " + "Bremer"
+
+#2
+num = 1234
+
+thousands = num / 1000
+hundreds = num % 1000 / 100
+tens = num % 100 / 10
+ones = num % 10
+
+puts thousands
+puts hundreds
+puts tens
+puts ones
+
+#3
+movies = {:movie1 => 2012, :movie2 => 2002, :movie3 => 2014, :movie4 => 2003, :movie5 => 2016}
+
+puts movies[:movie1]
+puts movies[:movie2]
+puts movies[:movie3]
+puts movies[:movie4]
+puts movies[:movie5]
+
+#4
+dates = [2012, 2002, 2014, 2003, 2016]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
+#5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#6
+
+puts 3.0 * 3.0
+puts 4.5 * 4.5
+puts 12.44 * 12.44
+
+#7 The is a ) missing in the program and a { without a }
