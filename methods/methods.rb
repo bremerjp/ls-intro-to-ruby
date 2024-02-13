7219078b934baf0fb0f14e98ba24cef341a0b416
Jacob Bremer <bremerjp@gmail.com>
2024-02-13 14:56:55 -0500

    finished methods

---
+++ methods/methods.rb
@@ -0,0 +1,20 @@
+#1
+
+def greeting(name)
+  "Hello, #{name}"
+end
+
+puts greeting("Jacob")
+
+#2
+# 2, nil, "Joe", false, nil
+
+#3
+def multiply(num1, num2)
+  num1 * num2
+end
+
+puts multiply(2, 4)
+
+#4 It will print nothing, return statement comes before the puts statementt.
+#5 There were not enough arguments passed into the method.
